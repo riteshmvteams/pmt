0c0d5890b38ab5defba942826e0363b1982c19d5
Ritesh <ritesh.mvteams@gmail.com>
2024-09-02 10:52:40 +0530

    refactor

--- src/app/(login)/page.tsx
+++ src/app/(login)/page.tsx
@@ -3,12 +3,12 @@ import LoginForm from "./_components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
-      <div className="flex items-center justify-center py-12">
+    <div className="w-full min-h-screen grid lg:grid-cols-2 place-content-center lg:place-content-stretch">
+      <div className="flex items-center justify-center py-12 px-6 lg:px-0">
         <div className="mx-auto grid max-w-[500px] w-full gap-20">
           <div className="grid text-left">
             <Logo url="/" />
-            <h1 className="text-3xl font-bold font-lexend">
+            <h1 className="text-xl md:text-3xl font-bold font-lexend">
               Welcome back! Please Log in to continue.
             </h1>
           </div>
