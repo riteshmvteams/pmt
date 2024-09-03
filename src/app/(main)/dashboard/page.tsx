c36b97d51a29b9c8ba5c5dc0f5cc6ab6dc229f25
Ritesh <ritesh.mvteams@gmail.com>
2024-09-03 12:37:44 +0530

    add dashboard charts

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import BarChartUI from "@/components/shared/chart/BarChart";
+import PieChartUI from "@/components/shared/chart/PieChart";
+import RadicalChartUI from "@/components/shared/chart/RadicalChart";
+import CheckInButton from "@/components/shared/CheckInButton";
 
 export default function Dashboard() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div className="flex items-center">
+      <div className="flex items-center justify-between">
         <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
+        <CheckInButton />
       </div>
-      <div
-        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-        x-chunk="dashboard-02-chunk-1"
-      >
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-bold tracking-tight">Empty</h3>
-        </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <BarChartUI className="sm:col-span-2 lg:col-span-3" />
+        <PieChartUI />
+        <RadicalChartUI />
+        <PieChartUI />
+        <PieChartUI />
+        <RadicalChartUI />
+        <PieChartUI />
+        <RadicalChartUI />
+        <PieChartUI />
+        <PieChartUI />
+        <RadicalChartUI />
       </div>
     </main>
   );
