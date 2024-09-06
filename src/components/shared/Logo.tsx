063d9ba9c69f7b8505a25ed5c62259b4904f8335
Ritesh <ritesh.mvteams@gmail.com>
2024-09-06 09:59:35 +0530

    update the new logo

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -5,12 +5,13 @@ export default function Logo({ url }: { url: string }) {
   return (
     <Link href={url} className="w-28 h-14">
       <Image
-        src="/images/companyLogo.png"
+        // src="/images/companyLogo.png"
+        src="/images/mvtLogo.png"
         alt="Image"
         width="1920"
         height="1080"
         priority
-        className="h-full w-full object-contain dark:brightness-[0.2] dark:grayscale"
+        className="h-full w-full object-contain scale-125 -ml-4"
       />
     </Link>
   );
