91a36072a3227b518f8986ecd5a7f4ba9d6fae84
Ritesh <ritesh.mvteams@gmail.com>
2024-10-10 16:56:47 +0530

    create time log filtering design

--- src/components/shared/timelog/TimeLogStats.tsx
+++ src/components/shared/timelog/TimeLogStats.tsx
@@ -14,6 +14,7 @@ import AddTime from "../addThings/AddTime";
 import { ModalTitle } from "@/components/sidebar/CreateNew";
 import { Button } from "@/components/ui/button";
 import { useDrawer } from "@/store/useDrawer";
+import TimeFiltering from "./TimeFiltering";
 
 export default function TimeLogStats() {
   return (
@@ -58,7 +59,7 @@ export const TimeLogAddFilter = () => {
         className="gap-2 font-lexend"
         onClick={() => {
           setDrawerTitle("Filter");
-          setDrawerChildren("Filter Here...F");
+          setDrawerChildren(<TimeFiltering />);
           setDrawerOpen(true);
           setSide("right");
           setDrawerClassName("");
