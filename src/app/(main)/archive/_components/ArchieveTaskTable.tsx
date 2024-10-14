49618d32d4014664585f2ab2819edc23ab6a0429
Ritesh <ritesh.mvteams@gmail.com>
2024-10-14 09:26:26 +0530

    add the confirmation modal with table bulk action button

--- src/app/(main)/archive/_components/ArchieveTaskTable.tsx
+++ src/app/(main)/archive/_components/ArchieveTaskTable.tsx
@@ -10,10 +10,11 @@ import { useMemo } from "react";
 import { PROJECTS_DATA } from "@/lib/constants";
 import { getColumns } from "./columns";
 import { useTable } from "@/hooks/useTable";
-import Text from "@/components/shared/Text";
 import { Button } from "@/components/ui/button";
 import { RotateCcw, Trash2 } from "lucide-react";
 import CustomTooltip from "@/components/shared/CustomTooltip";
+import { useModal } from "@/store/useModal";
+import ConfirmationModal from "@/components/shared/ConfirmationModal";
 
 export default function ArchieveTaskTable() {
   // get the columns data
@@ -32,22 +33,7 @@ export default function ArchieveTaskTable() {
 
   return (
     <div className="flex flex-col gap-2">
-      {getRowId()?.length ? (
-        <div className="bg-muted/50 px-4 py-2 rounded-md border flex justify-between items-center top-0 left-0 w-full">
-          <div className="flex items-center gap-2">
-            <CustomTooltip title="Restore Tasks">
-              <Button className="" variant="outline" size="icon">
-                <RotateCcw className="h-[14px] w-[14px]" />
-              </Button>
-            </CustomTooltip>
-            <CustomTooltip title="Delete Tasks">
-              <Button className="" variant="outline" size="icon">
-                <Trash2 className="h-[14px] w-[14px]" />
-              </Button>
-            </CustomTooltip>
-          </div>
-        </div>
-      ) : null}
+      {getRowId()?.length ? <BulkTableActions /> : null}
       <DataTable
         columns={columns}
         data={PROJECTS_DATA}
@@ -57,3 +43,53 @@ export default function ArchieveTaskTable() {
     </div>
   );
 }
+
+const BulkTableActions = () => {
+  const { setChildren, setClassName, setOpen, setTitle } = useModal();
+  return (
+    <div className="bg-muted/50 px-4 py-2 rounded-md border flex justify-between items-center top-0 left-0 w-full">
+      <div className="flex items-center gap-2">
+        <CustomTooltip title="Restore Tasks">
+          <Button
+            className=""
+            variant="outline"
+            size="icon"
+            onClick={() => {
+              setOpen(true);
+              setTitle("Restore > Project Title");
+              setClassName("max-w-[600px]");
+              setChildren(
+                <ConfirmationModal
+                  title="Are you Sure? You want to Restore the Task"
+                  name="Task Name"
+                />
+              );
+            }}
+          >
+            <RotateCcw className="h-[14px] w-[14px]" />
+          </Button>
+        </CustomTooltip>
+        <CustomTooltip title="Delete Tasks">
+          <Button
+            className=""
+            variant="outline"
+            size="icon"
+            onClick={() => {
+              setOpen(true);
+              setTitle("Delete > Project Title");
+              setClassName("max-w-[600px]");
+              setChildren(
+                <ConfirmationModal
+                  title="Are you Sure? You want to Remove the Task from Archeive List"
+                  name="Task Name"
+                />
+              );
+            }}
+          >
+            <Trash2 className="h-[14px] w-[14px]" />
+          </Button>
+        </CustomTooltip>
+      </div>
+    </div>
+  );
+};
